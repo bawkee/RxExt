641d0255bdc20cf359c1fbc035017bcf9061ef86
Bojan Sala <bojan.sala@yahoo.com>
2023-11-17 16:27:29 +0100

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# rxext
-Helper Extensions for System.Reactive
+# RxExt
+Simple but extremely useful helper extensions for System.Reactive.
+
+Some of the extension methods include:
+
+- PairWithPrevious
+- SeriallyDispose
+- MaxSoFar / MinSoFar
+- Invert
+- ThrottleBuffer
+- Throttle with interval selector
+- StartWith that works correctly™ with enumerables
+- RetryWhile a RetryWhen with a twist
+- OnSubscribed lets you do things when subscribed
+- EnsureAction makes sure an action is done no matter what
+- WhereNotNull
+-ab
---
+++ Source/RxExt/EnsuredActionObserver.cs
@@ -0,0 +1,29 @@
+﻿namespace RxExt
+{
+    using System;
+
+    /// <summary>
+    /// Wrapper for the <see cref="RxExtensions.EnsureAction"/> extension result.
+    /// </summary>
+    public class EnsuredActionObserver<T> : IObserver<T>
+    {
+        private bool _empty = true;
+        private readonly Action _action;
+
+        public EnsuredActionObserver(Action action) { _action = action; }
+
+        public void OnCompleted()
+        {
+            if (_empty)
+                _action();
+        }
+
+        public void OnError(Exception error) { _action(); }
+
+        public void OnNext(T value)
+        {
+            _empty = false;
+            _action();
+        }
+    }
+}
